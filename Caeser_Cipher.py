a02ad81c7b6965be8917728dd8cc1bbb8b24bba2
SPdrag0n <69705177+programmer24680@users.noreply.github.com>
2023-07-22 08:27:30 +0530

    Update Caeser_Cipher.py

--- Caeser_Cipher.py
+++ Caeser_Cipher.py
@@ -15,18 +15,6 @@ def encode(message):
         decode_key.append(str(key_value))
         ascii_list.append(new_ascii)
         message_letter_list[i] = new_ascii
-        # if 97 <= message_letter_list[i] <= 122:
-        #     while new_ascii > 122:
-        #         new_ascii = element_ascii + key - 123 + 97
-        #     message_letter_list[i] = new_ascii
-        #     # else:
-        #     #     message_letter_list[i] = new_ascii
-        # elif 65 <= message_letter_list[i] <= 90:
-        #     while new_ascii > 90:
-        #         new_ascii = element_ascii + key - 91 + 65
-        #     message_letter_list[i] = new_ascii
-        # else:
-        #     pass
     new_message = ""
     for element in message_letter_list:
         new_message += chr(element)
@@ -56,34 +44,6 @@ def decode(message, decodeKey):
     for element in message_letter_list:
         new_message += chr(element)
     print(new_message)
-
-
-
-
-# def decode(message):
-#     message_letter_list = []
-#     for i in range(0, len(message)):
-#         message_letter_list.append(ord(message[i]))
-#         element_ascii = message_letter_list[i]
-#         new_ascii = element_ascii - shift_value
-#         if 97 <= message_letter_list[i] <= 122:
-#             while new_ascii < 97:
-#                 new_ascii = element_ascii - shift_value + 123 - 97
-#             message_letter_list[i] = new_ascii
-#             # else:
-#             #     message_letter_list[i] = new_ascii
-#         elif 65 <= message_letter_list[i] <= 90:
-#             while new_ascii < 65:
-#                 new_ascii = element_ascii - shift_value + 91 - 65
-#             message_letter_list[i] = new_ascii
-#         else:
-#             pass
-#     new_message = ""
-#     for element in message_letter_list:
-#         new_message += chr(element)
-#     print(new_message)
-
-
 print("Welcome to caeser cipher program.")
 command = input("Type e for encode and d for decode: ").lower()
 if command == "e":
